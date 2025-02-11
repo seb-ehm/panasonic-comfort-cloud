147edd7f94b07d340b390be49ffd4b436ae7bb7f
seb-ehm <sebastian@dipolmoment.de>
2025-02-11 22:50:18 +0100

    Add auth related consts

--- comfortcloud/constants.go
+++ comfortcloud/constants.go
@@ -1,1 +1,10 @@
 package comfortcloud
+
+const (
+	AppClientId  = "Xmy6xIYIitMxngjB2rHvlm6HSDNnaMJx"
+	Auth0Client  = "eyJuYW1lIjoiQXV0aDAuQW5kcm9pZCIsImVudiI6eyJhbmRyb2lkIjoiMzAifSwidmVyc2lvbiI6IjIuOS4zIn0="
+	RedirectUri  = "panasonic-iot-cfc://authglb.digital.panasonic.com/android/com.panasonic.ACCsmart/callback"
+	BasePathAuth = "https://authglb.digital.panasonic.com"
+	BasePathAcc  = "https://accsmart.panasonic.com"
+	XAppVersion  = "1.22.0"
+)
